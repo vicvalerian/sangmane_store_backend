b4078ce2a63c2274255baff6e13ade99ec70cf50
Vicky Valerian <vickyvalerian27@gmail.com>
2023-11-10 23:48:18 +0800

    Update Slider

--- app/Http/Controllers/Backend/SliderController.php
+++ app/Http/Controllers/Backend/SliderController.php
@@ -33,7 +33,7 @@ class SliderController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            // 'banner' => ['required', 'image', 'max:2000'],
+            'banner' => ['required', 'image', 'max:2000'],
             'type' => ['string', 'max:200'],
             'title' => ['required', 'max:200'],
             'starting_price' => ['max:200'],
@@ -56,9 +56,9 @@ class SliderController extends Controller
         $slider->status = $request->status;
         $slider->save();
 
-        toastr('Slider Created Successfully!', 'success');
+        toastr('Slider Updated Successfully!', 'success');
 
-        return redirect()->back();
+        return redirect()->route('admin.slider.index');
     }
 
     /**
@@ -74,7 +74,8 @@ class SliderController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $slider = Slider::findOrFail($id);
+        return view('admin.slider.edit', compact('slider'));
     }
 
     /**
@@ -82,7 +83,33 @@ class SliderController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'banner' => ['nullable', 'image', 'max:2000'],
+            'type' => ['string', 'max:200'],
+            'title' => ['required', 'max:200'],
+            'starting_price' => ['max:200'],
+            'btn_url' => ['url'],
+            'serial' => ['required'],
+            'status' => ['required'],
+        ]);
+
+        $slider = Slider::findOrFail($id);
+
+        // Handle File Upload
+        $imagePath = $this->updateImage($request, 'banner', 'uploads', $slider->banner);
+
+        $slider->banner = empty(!$imagePath) ? $imagePath : $slider->banner;
+        $slider->type = $request->type;
+        $slider->title = $request->title;
+        $slider->starting_price = $request->starting_price;
+        $slider->btn_url = $request->btn_url;
+        $slider->serial = $request->serial;
+        $slider->status = $request->status;
+        $slider->save();
+
+        toastr('Slider Created Successfully!', 'success');
+
+        return redirect()->route('admin.slider.index');
     }
 
     /**
