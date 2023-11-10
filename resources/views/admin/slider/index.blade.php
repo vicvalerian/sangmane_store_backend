744278dc4be997cbf027bf9edf28a0fb8808435e
Vicky Valerian <vickyvalerian27@gmail.com>
2023-11-11 01:55:18 +0800

    Slider - Fixing and Improvement

--- resources/views/admin/slider/index.blade.php
+++ resources/views/admin/slider/index.blade.php
@@ -3,12 +3,7 @@
 @section('content')
     <section class="section">
         <div class="section-header">
-            <h1>Table</h1>
-            <div class="section-header-breadcrumb">
-                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-                <div class="breadcrumb-item"><a href="#">Components</a></div>
-                <div class="breadcrumb-item">Table</div>
-            </div>
+            <h1>Slider</h1>
         </div>
 
         <div class="section-body">
@@ -16,9 +11,10 @@
                 <div class="col-12 col-md-12 col-lg-12">
                     <div class="card">
                         <div class="card-header">
-                            <h4>Simple Table</h4>
+                            <h4>List of Slider</h4>
                             <div class="card-header-action">
-                                <a href="{{ route('admin.slider.create') }}" class="btn btn-primary">Add Slider</a>
+                                <a href="{{ route('admin.slider.create') }}" class="btn btn-primary"><i
+                                        class="fas fa-plus"></i> Add Slider</a>
                             </div>
                         </div>
                         <div class="card-body">
