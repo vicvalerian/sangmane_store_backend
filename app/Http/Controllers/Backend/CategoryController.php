f46770d13c9a1d37a47bb3fff7acb52366557a72
Vicky Valerian <vickyvalerian27@gmail.com>
2023-11-11 14:41:20 +0800

    Update & Delete Category

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -62,7 +62,8 @@ class CategoryController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $category = Category::findOrFail($id);
+        return view('admin.category.edit', compact('category'));
     }
 
     /**
@@ -70,7 +71,23 @@ class CategoryController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'icon' => ['required', 'not_in:empty'],
+            'name' => ['required', 'max:200', 'unique:categories,name,' . $id],
+            'status' => ['required'],
+        ]);
+
+        $category = Category::findOrFail($id);
+
+        $category->icon = $request->icon;
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
+        $category->status = $request->status;
+        $category->save();
+
+        toastr('Category Updated Successfully!', 'success');
+
+        return redirect()->route('admin.category.index');
     }
 
     /**
@@ -78,6 +95,12 @@ class CategoryController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $category = Category::findOrFail($id);
+        $category->delete();
+
+        return response([
+            'status' => 'success',
+            'message' => 'Category Deleted Successfully!'
+        ]);
     }
 }
---
+++ resources/views/admin/category/edit.blade.php
@@ -0,0 +1,47 @@
+@extends('admin.layouts.master')
+
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>Category</h1>
+        </div>
+
+        <div class="section-body">
+            <div class="row">
+                <div class="col-12 col-md-12 col-lg-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Update Category</h4>
+                        </div>
+                        <div class="card-body">
+                            <form action="{{ route('admin.category.update', $category->id) }}" method="post">
+                                @csrf
+                                @method('PUT')
+                                <div class="form-group">
+                                    <label>Icon</label>
+                                    <div>
+                                        <button class="btn btn-primary" role="iconpicker" data-selected-class="btn-danger"
+                                            data-unselected-class="btn-info" name="icon"
+                                            data-icon="{{ $category->icon }}"></button>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label>Name</label>
+                                    <input type="text" class="form-control" name="name" value="{{ $category->name }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputState">Status</label>
+                                    <select id="inputState" class="form-control" name="status">
+                                        <option {{ $category->status == 1 ? 'selected' : '' }} value="1">Active</option>
+                                        <option {{ $category->status == 0 ? 'selected' : '' }} value="0">Inactive
+                                    </select>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
