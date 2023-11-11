32dddda455fd8752331284761ebe4e682ffcac71
Vicky Valerian <vickyvalerian27@gmail.com>
2023-11-11 16:02:02 +0800

    Delete & Change Status Sub Category

--- app/Http/Controllers/Backend/SubCategoryController.php
+++ app/Http/Controllers/Backend/SubCategoryController.php
@@ -97,6 +97,23 @@ class SubCategoryController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $subCategory = SubCategory::findOrFail($id);
+        $subCategory->delete();
+
+        return response([
+            'status' => 'success',
+            'message' => 'Sub Category Deleted Successfully!'
+        ]);
+    }
+
+    public function changeStatus(Request $request)
+    {
+        $subCategory = SubCategory::findOrFail($request->id);
+        $subCategory->status = $request->status == 'true' ? 1 : 0;
+        $subCategory->save();
+
+        return response([
+            'message' => 'Status has been updated!',
+        ]);
     }
 }
--- resources/views/admin/sub-category/index.blade.php
+++ resources/views/admin/sub-category/index.blade.php
@@ -37,7 +37,7 @@
                 let id = $(this).data('id');
 
                 $.ajax({
-                    url: "{{route('admin.category.change-status')}}",
+                    url: "{{route('admin.sub-category.change-status')}}",
                     method: 'PUT',
                     data: {
                         status: isChecked,
