5325eb4472c1d0146b5785373682c56426804882
Vicky Valerian <vickyvalerian27@gmail.com>
2023-11-20 16:31:42 +0800

    storing paypal order, order prod, transaction

--- app/Http/Controllers/Frontend/PaymentController.php
+++ app/Http/Controllers/Frontend/PaymentController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\GeneralSetting;
+use App\Models\Order;
+use App\Models\OrderProduct;
 use App\Models\PaypalSetting;
+use App\Models\Product;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Srmklive\PayPal\Services\PayPal as PayPalClient;
 
@@ -21,6 +27,65 @@ class PaymentController extends Controller
     public function paymentSuccess()
     {
         return view('frontend.pages.payment-success');
+    }
+
+    public function storeOrder($paymentMethod, $paymentStatus, $transactionId, $paidAmount, $paidCurrencyName)
+    {
+        $setting = GeneralSetting::first();
+
+        $order = new Order();
+        $order->invoice_id = rand(1, 999999);
+        $order->user_id = Auth::user()->id;
+        $order->sub_total = getCartTotal();
+        $order->amount =  getFinalPayableAmount();
+        $order->currency_name = $setting->currency_name;
+        $order->currency_icon = $setting->currency_icon;
+        $order->product_qty = \Cart::content()->count();
+        $order->payment_method = $paymentMethod;
+        $order->payment_status = $paymentStatus;
+        $order->order_address = json_encode(Session::get('address'));
+        $order->shpping_method = json_encode(Session::get('shipping_method'));
+        $order->coupon = json_encode(Session::get('coupon'));
+        $order->order_status = 'pending';
+        $order->save();
+
+        // store order products
+        foreach (\Cart::content() as $item) {
+            $product = Product::find($item->id);
+            $orderProduct = new OrderProduct();
+            $orderProduct->order_id = $order->id;
+            $orderProduct->product_id = $product->id;
+            $orderProduct->vendor_id = $product->vendor_id;
+            $orderProduct->product_name = $product->name;
+            $orderProduct->variants = json_encode($item->options->variants);
+            $orderProduct->variant_total = $item->options->variants_total;
+            $orderProduct->unit_price = $item->price;
+            $orderProduct->qty = $item->qty;
+            $orderProduct->save();
+
+            // update product quantity
+            $updatedQty = ($product->qty - $item->qty);
+            $product->qty = $updatedQty;
+            $product->save();
+        }
+
+        // store transaction details
+        $transaction = new Transaction();
+        $transaction->order_id = $order->id;
+        $transaction->transaction_id = $transactionId;
+        $transaction->payment_method = $paymentMethod;
+        $transaction->amount = getFinalPayableAmount();
+        $transaction->amount_real_currency = $paidAmount;
+        $transaction->amount_real_currency_name = $paidCurrencyName;
+        $transaction->save();
+    }
+
+    public function clearSession()
+    {
+        \Cart::destroy();
+        Session::forget('address');
+        Session::forget('shipping_method');
+        Session::forget('coupon');
     }
 
     public function paypalConfig()
@@ -97,15 +162,15 @@ class PaymentController extends Controller
         $response = $provider->capturePaymentOrder($request->token);
 
         if (isset($response['status']) && $response['status'] == 'COMPLETED') {
-            // // calculate payable amount depending on currency rate
-            // $paypalSetting = PaypalSetting::first();
-            // $total = getFinalPayableAmount();
-            // $paidAmount = round($total * $paypalSetting->currency_rate, 2);
+            // calculate payable amount depending on currency rate
+            $paypalSetting = PaypalSetting::first();
+            $total = getFinalPayableAmount();
+            $paidAmount = round($total * $paypalSetting->currency_rate, 2);
 
-            // $this->storeOrder('paypal', 1, $response['id'], $paidAmount, $paypalSetting->currency_name);
+            $this->storeOrder('paypal', 1, $response['id'], $paidAmount, $paypalSetting->currency_name);
 
-            // // clear session
-            // $this->clearSession();
+            // clear session
+            $this->clearSession();
 
             return redirect()->route('user.payment.success');
         }
