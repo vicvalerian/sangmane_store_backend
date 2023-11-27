3788125de1b2961af115107decaf52e766dbb2f0
Vicky Valerian <vickyvalerian27@gmail.com>
2023-11-27 15:28:11 +0800

    admin dashboard footer

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -80,8 +80,7 @@
             </div>
             <footer class="main-footer">
                 <div class="footer-left">
-                    Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad
-                        Nauval Azhar</a>
+                    Copyright &copy; 2023 <div class="bullet"></div> Created By <a href="javascript:;">Vicky Valerian</a>
                 </div>
                 <div class="footer-right">
 
